faa7a89b3a7c47cbc568b927fa93837fa622a251
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-03-14 10:40:52 +0100

    blog-000 Main Layout és Post List Component

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+const postList = [
+    {
+        author: 'John Doe',
+        date: '2020.03.14',
+        title: 'First Post',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ea! Animi, et. Magni dolorem esse recusandae architecto deserunt. Consectetur tempore soluta ex sunt suscipit deserunt reprehenderit minus nihil, commodi quae.'
+    },
+    {
+        author: 'John Davis',
+        date: '2020.03.12',
+        title: 'Second Post',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ea! Animi, et. Magni dolorem esse recusandae architecto deserunt. Consectetur tempore soluta ex sunt suscipit deserunt reprehenderit minus nihil, commodi quae.'
+    },
+    {
+        author: 'Jimmy Doe',
+        date: '2020.03.10',
+        title: 'Lorem Post',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ea! Animi, et. Magni dolorem esse recusandae architecto deserunt. Consectetur tempore soluta ex sunt suscipit deserunt reprehenderit minus nihil, commodi quae.'
+    },
+    {
+        author: 'John Doe',
+        date: '2020.03.14',
+        title: 'First Post',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ea! Animi, et. Magni dolorem esse recusandae architecto deserunt. Consectetur tempore soluta ex sunt suscipit deserunt reprehenderit minus nihil, commodi quae.'
+    },
+    {
+        author: 'John Doe',
+        date: '2020.03.14',
+        title: 'First Post',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ea! Animi, et. Magni dolorem esse recusandae architecto deserunt. Consectetur tempore soluta ex sunt suscipit deserunt reprehenderit minus nihil, commodi quae.'
+    }
+]
+
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        siteTitle: 'Bishops First Blog',
+        postList
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
