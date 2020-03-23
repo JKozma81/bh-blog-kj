1b4ad3e83ce6057425b3792c5178c345b0d94cd6
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-03-23 06:22:37 +0100

    quick fix

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.get('/', async (req, res) => {
 		sortedBlogPosts.forEach(blogPost => {
 			let fullDate = blogPost.published_at.split(' ')[0].split('-');
 			fullDate = fullDate.map(element => (element = Number(element)));
+			fullDate[1] -= 1;
 			let date = new Date(...fullDate);
 			let year = date.getFullYear();
 			let month = date.toLocaleString('en-US', { month: 'long' });
