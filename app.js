7880c14487bc311afbfd334840f03eb7c535230e
JKozma81 <janos.kozma81@gmail.com>
2020-03-24 18:40:05 +0100

    moving master root to the postcontroller

--- app.js
+++ app.js
@@ -1,83 +1,68 @@
-const express = require('express');
-const path = require('path');
-const hbs = require('express-handlebars');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const path = require("path");
+const hbs = require("express-handlebars");
+const cookieParser = require("cookie-parser");
 
-const LoginController = require('./controllers/LoginController');
-const AdminController = require('./controllers/AdminController');
-const UserAuthenticationMiddleware = require('./middlewares/Authentication');
-const PostController = require('./controllers/PostController');
-const PostsDAO = require('./DAO/PostsDAO');
-const DataFormatingService = require('./services/DataFormatingService');
+const LoginController = require("./controllers/LoginController");
+const AdminController = require("./controllers/AdminController");
+const UserAuthenticationMiddleware = require("./middlewares/Authentication");
+const PostController = require("./controllers/PostController");
+const PostsDAO = require("./DAO/PostsDAO");
 
 const app = express();
 const port = 3000;
 
-app.engine('handlebars', hbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs());
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-app.get('/', async (req, res) => {
-	try {
-		const sortedBlogPosts = await PostsDAO.getPublishedPostSortedByDate();
+app.get("/", PostController.getMasterRoot);
 
-		res.render('home', {
-			siteTitle: 'Bishops First Blog',
-			postList: await PostsDAO.getAllPublishedPosts(),
-			formatedBlogPostData: DataFormatingService.formatDataForArchive(
-				sortedBlogPosts
-			)
-		});
-	} catch (err) {
-		console.error(err);
-	}
-});
+app.get("/login", LoginController.get);
 
-app.get('/login', LoginController.get);
+app.post("/login", LoginController.post, LoginController.logUserIn);
 
-app.post('/login', LoginController.post, LoginController.logUserIn);
-
-app.get('/logout', LoginController.logUserOut);
+app.get("/logout", LoginController.logUserOut);
 
 app.get(
-	'/admin',
-	UserAuthenticationMiddleware.authenticate,
-	AdminController.get
+  "/admin",
+  UserAuthenticationMiddleware.authenticate,
+  AdminController.get
 );
 
 app.get(
-	'/admin/list',
-	UserAuthenticationMiddleware.authenticate,
-	AdminController.adminBlogPostList
+  "/admin/list",
+  UserAuthenticationMiddleware.authenticate,
+  AdminController.adminBlogPostList
 );
 
 app.get(
-	'/admin/list/:id',
-	UserAuthenticationMiddleware.authenticate,
-	AdminController.editBlogPost
+  "/admin/list/:id",
+  UserAuthenticationMiddleware.authenticate,
+  AdminController.editBlogPost
 );
 
 app.post(
-	'/admin/list/:id',
-	UserAuthenticationMiddleware.authenticate,
-	AdminController.modifyBlogPost
+  "/admin/list/:id",
+  UserAuthenticationMiddleware.authenticate,
+  AdminController.modifyBlogPost
 );
 
 app.get(
-	'/posts',
-	UserAuthenticationMiddleware.authenticate,
-	PostController.get
+  "/posts",
+  UserAuthenticationMiddleware.authenticate,
+  PostController.get
 );
 
-app.get('/posts/:idOrSlug', PostController.showBlogPost);
+app.get("/posts/:idOrSlug", PostController.showBlogPost);
 
 app.post(
-	'/posts',
-	UserAuthenticationMiddleware.authenticate,
-	PostController.post
+  "/posts",
+  UserAuthenticationMiddleware.authenticate,
+  PostController.post
 );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,89 +1,104 @@
-const PostsDAO = require('../DAO/PostsDAO');
-const CookieService = require('../services/CookieService');
-const SessionServices = require('../services/SessionServices');
-const MessageProviderService = require('../services/MessageProviderService');
+const PostsDAO = require("../DAO/PostsDAO");
+const CookieService = require("../services/CookieService");
+const SessionServices = require("../services/SessionServices");
+const MessageProviderService = require("../services/MessageProviderService");
+const DataFormatingService = require("../services/DataFormatingService");
 
 class PostController {
-	static get(req, res) {
-		const user = req.user;
-		let postError = req.query.error
-			? MessageProviderService.getMessage(req.query.error)
-			: '';
+  static async getMasterRoot(req, res) {
+    try {
+      const sortedBlogPosts = await PostsDAO.getPublishedPostSortedByDate();
 
-		res.render('newPost', {
-			siteTitle: 'Bishops First Blog',
-			submenuTitle: 'New Post',
-			username: user.username,
-			postError,
-			title: req.query.title ? req.query.title : '',
-			content: req.query.content ? req.query.content : '',
-			slug: req.query.slug ? req.query.slug : ''
-		});
-	}
+      res.render("home", {
+        siteTitle: "Bishops First Blog",
+        postList: await PostsDAO.getAllPublishedPosts(),
+        formatedBlogPostData: DataFormatingService.formatDataForArchive(
+          sortedBlogPosts
+        )
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-	static async post(req, res) {
-		const { title, content, slug, draft } = req.body;
+  static get(req, res) {
+    const user = req.user;
+    let postError = req.query.error
+      ? MessageProviderService.getMessage(req.query.error)
+      : "";
 
-		if (!title && !content && !slug) {
-			res.redirect('/posts?error=title-content-slug');
-			return;
-		}
-		if (!title && !slug) {
-			res.redirect(`/posts?error=title-and-slug&content=${content}`);
-			return;
-		}
-		if (!slug && !content) {
-			res.redirect(`/posts?error=content-and-slug&title=${title}`);
-			return;
-		}
-		if (!title) {
-			res.redirect(`/posts?error=title&content=${content}&slug=${slug}`);
-			return;
-		}
-		if (!content) {
-			res.redirect(`/posts?error=content&title=${title}&slug=${slug}`);
-			return;
-		}
-		if (!slug) {
-			res.redirect(
-				`/posts?error=content&title=${title}&content=${content}`
-			);
-			return;
-		}
+    res.render("newPost", {
+      siteTitle: "Bishops First Blog",
+      submenuTitle: "New Post",
+      username: user.username,
+      postError,
+      title: req.query.title ? req.query.title : "",
+      content: req.query.content ? req.query.content : "",
+      slug: req.query.slug ? req.query.slug : ""
+    });
+  }
 
-		const SID = Number(req.cookies[CookieService.getCookie()]);
-		const user = SessionServices.getSession(SID).user;
+  static async post(req, res) {
+    const { title, content, slug, draft } = req.body;
 
-		const newPost = {
-			title,
-			author: user.username,
-			content,
-			slug,
-			draft
-		};
-		await PostsDAO.addPost(newPost);
-		res.redirect('/admin');
-	}
+    if (!title && !content && !slug) {
+      res.redirect("/posts?error=title-content-slug");
+      return;
+    }
+    if (!title && !slug) {
+      res.redirect(`/posts?error=title-and-slug&content=${content}`);
+      return;
+    }
+    if (!slug && !content) {
+      res.redirect(`/posts?error=content-and-slug&title=${title}`);
+      return;
+    }
+    if (!title) {
+      res.redirect(`/posts?error=title&content=${content}&slug=${slug}`);
+      return;
+    }
+    if (!content) {
+      res.redirect(`/posts?error=content&title=${title}&slug=${slug}`);
+      return;
+    }
+    if (!slug) {
+      res.redirect(`/posts?error=content&title=${title}&content=${content}`);
+      return;
+    }
 
-	static async showBlogPost(req, res) {
-		const searchParameter = Number(req.params.idOrSlug);
+    const SID = Number(req.cookies[CookieService.getCookie()]);
+    const user = SessionServices.getSession(SID).user;
 
-		const blogPost = await PostsDAO.getPost(
-			isNaN(searchParameter) ? req.params.idOrSlug : searchParameter
-		);
+    const newPost = {
+      title,
+      author: user.username,
+      content,
+      slug,
+      draft
+    };
+    await PostsDAO.addPost(newPost);
+    res.redirect("/admin");
+  }
 
-		if (!blogPost) {
-			res.render('custom404', {
-				siteTitle: 'Bishops First Blog'
-			});
-			return;
-		}
+  static async showBlogPost(req, res) {
+    const searchParameter = Number(req.params.idOrSlug);
 
-		res.render('readPost', {
-			siteTitle: 'Bishops First Blog',
-			post: blogPost
-		});
-	}
+    const blogPost = await PostsDAO.getPost(
+      isNaN(searchParameter) ? req.params.idOrSlug : searchParameter
+    );
+
+    if (!blogPost) {
+      res.render("custom404", {
+        siteTitle: "Bishops First Blog"
+      });
+      return;
+    }
+
+    res.render("readPost", {
+      siteTitle: "Bishops First Blog",
+      post: blogPost
+    });
+  }
 }
 
 module.exports = PostController;
